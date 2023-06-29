b35aa191d2ea171c5ac7b51c33eb6dbbdc864bad
Luke Chen <showuon@gmail.com>
2023-06-29 15:16:13 +0800

    improving zk health check

--- docker-images/kafka-based/kafka/scripts/zookeeper_healthcheck.sh
+++ docker-images/kafka-based/kafka/scripts/zookeeper_healthcheck.sh
@@ -3,7 +3,11 @@ set -e
 
 OK=$(echo ruok | nc 127.0.0.1 12181)
 if [ "$OK" == "imok" ]; then
-    exit 0
-else
-    exit 1
+    # checking if the node is a follower or a leader, otherwise, it's not ready
+    SRVR=$(echo srvr | nc 127.0.0.1 12181 | grep "Mode:" | grep "leader\|follower")
+    if [ ! -z "$SRVR" ]; then
+        exit 0
+    fi
 fi
+
+exit 1
