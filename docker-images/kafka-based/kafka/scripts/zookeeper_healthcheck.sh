1bb573310d0416593f36c19dfcafe14e119dcf74
Luke Chen <showuon@gmail.com>
2023-06-29 15:37:22 +0800

    use -n

--- docker-images/kafka-based/kafka/scripts/zookeeper_healthcheck.sh
+++ docker-images/kafka-based/kafka/scripts/zookeeper_healthcheck.sh
@@ -5,7 +5,7 @@ OK=$(echo ruok | nc 127.0.0.1 12181)
 if [ "$OK" == "imok" ]; then
     # checking if the node is a follower or a leader, otherwise, it's not ready
     SRVR=$(echo srvr | nc 127.0.0.1 12181 | grep "Mode:" | grep "leader\|follower")
-    if [ ! -z "$SRVR" ]; then
+    if [ -n "$SRVR" ]; then
         exit 0
     fi
 fi
