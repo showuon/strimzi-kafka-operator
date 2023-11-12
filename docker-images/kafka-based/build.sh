28647355006f74d3ec2e4cb1ecccf7b1bb0ab98d
Your Name <you@example.com>
2023-11-12 13:24:43 +0800

    update kafka image

--- docker-images/kafka-based/build.sh
+++ docker-images/kafka-based/build.sh
@@ -57,7 +57,7 @@ function build {
         for image in $kafka_images
         do
             make -C "$image" "$targets" \
-                DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg KAFKA_VERSION=${kafka_version} --build-arg KAFKA_DIST_DIR=${relative_dist_dir} --build-arg THIRD_PARTY_LIBS=${lib_directory} $(alternate_base "$image")" \
+                DOCKER_BUILD_ARGS="$DOCKER_BUILD_ARGS --build-arg KAFKA_LIBS_DIR=./tmp/libs/ --build-arg KAFKA_VERSION=${kafka_version} --build-arg KAFKA_DIST_DIR=${relative_dist_dir} --build-arg THIRD_PARTY_LIBS=${lib_directory} $(alternate_base "$image")" \
                 DOCKER_TAG="${tag}-kafka-${kafka_version}" \
                 BUILD_TAG="build-kafka-${kafka_version}" \
                 KAFKA_VERSION="${kafka_version}" \
