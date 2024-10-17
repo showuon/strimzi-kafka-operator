a4092315c0a141421723d606af53ef0fbee742c7
Luke Chen <showuon@gmail.com>
2024-10-17 18:19:11 +0800

    test

--- systemtest/src/main/java/io/strimzi/systemtest/templates/crd/KafkaTemplates.java
+++ systemtest/src/main/java/io/strimzi/systemtest/templates/crd/KafkaTemplates.java
@@ -366,6 +366,16 @@ public class KafkaTemplates {
                 .endKafka()
                 .editZookeeper()
                     .withReplicas(zookeeperReplicas)
+                    .addToConfig("ssl.protocol", "TLSv1.3")
+                    .addToConfig("ssl.quorum.protocol", "TLSv1.3")
+                    .addToConfig("ssl.enabledProtocols", "TLSv1.3,TLSv1.2")
+                    .addToConfig("ssl.quorum.enabledProtocols", "TLSv1.3,TLSv1.2")
+                    .addToConfig("ssl.ciphersuites", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_\n" +
+                            "AES_128_CBC_SHA256,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_RSA_WITH_AES_256_CBC_S\n" +
+                            "HA384,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,TLS_AES_256_GCM_SHA384,TLS_AES_128_GCM_SHA256,TLS_CHACHA20_POLY1305_SHA256")
+                    .addToConfig("ssl.quorum.ciphersuites", "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_ECDSA_WITH_\n" +
+                            "AES_128_CBC_SHA256,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_RSA_WITH_AES_256_CBC_S\n" +
+                            "HA384,TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,TLS_AES_256_GCM_SHA384,TLS_AES_128_GCM_SHA256,TLS_CHACHA20_POLY1305_SHA256")
                 .endZookeeper()
             .endSpec();
     }
