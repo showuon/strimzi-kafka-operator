2d1fea79ad6f0a6a8ef61a0ef24fc859466ee6bf
Luke Chen <showuon@gmail.com>
2025-04-15 17:32:35 +0800

    test

--- systemtest/src/test/java/io/strimzi/systemtest/kafka/TieredStorageST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/kafka/TieredStorageST.java
@@ -386,7 +386,7 @@ public class TieredStorageST extends AbstractST {
         NamespaceManager.getInstance().createNamespaceAndPrepare(suiteStorage.getNamespaceName());
         cluster.setNamespace(suiteStorage.getNamespaceName());
 
-        ImageBuild.buildImage(suiteStorage.getNamespaceName(), IMAGE_NAME, TIERED_STORAGE_DOCKERFILE, BUILT_IMAGE_TAG, Environment.KAFKA_TIERED_STORAGE_BASE_IMAGE);
+//        ImageBuild.buildImage(suiteStorage.getNamespaceName(), IMAGE_NAME, TIERED_STORAGE_DOCKERFILE, BUILT_IMAGE_TAG, Environment.KAFKA_TIERED_STORAGE_BASE_IMAGE);
 
 //        this.clusterOperator = this.clusterOperator
 //            .defaultInstallation()
