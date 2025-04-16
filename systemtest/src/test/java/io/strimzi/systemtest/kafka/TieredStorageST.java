a123c51f90d98bb472ad5f1a8daf72a6081ac5f6
Luke Chen <showuon@gmail.com>
2025-04-16 10:17:47 +0800

    10 sec

--- systemtest/src/test/java/io/strimzi/systemtest/kafka/TieredStorageST.java
+++ systemtest/src/test/java/io/strimzi/systemtest/kafka/TieredStorageST.java
@@ -348,6 +348,11 @@ public class TieredStorageST extends AbstractST {
      */
     private void deployNfsInstance() {
         LOGGER.info("=== Deploying NFS instance ===");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
         // allow NetworkPolicies for the NFS in case that we have "default to deny all" mode enabled
         NetworkPolicyResource.allowNetworkPolicyAllIngressForMatchingLabel(suiteStorage.getNamespaceName(), "nfs", Map.of(TestConstants.APP_POD_LABEL, "nfs-server-provisioner"));
